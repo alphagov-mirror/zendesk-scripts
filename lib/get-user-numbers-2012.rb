658c8394ae1fbd50946b38f554d888ff5610c792
David Pye <david.pye@digital.cabinet-office.gov.uk>
2019-10-09 12:58:36 +0100

    add build and run pipelines for group 21080146 to sandbox concourse

--- lib/get-user-numbers-2012.rb
+++ lib/get-user-numbers-2012.rb
@@ -1,24 +1,18 @@
 require 'zendesk_api'
 
-@client = ZendeskAPI::Client.new do |config|
-  config.url = ENV['ZENDESK_URL']
-  config.username = ENV['ZENDESK_USER_EMAIL']
-  config.password = ENV['ZENDESK_USER_PASSWORD']
-
-  config.retry = true
-end
+require_relative 'zendesk-setup.rb'
 
 #
 # 2012
 #
 
-@y2012_users = []
+@y2012_tickets = []
 
 (1..140).each do |i|
-  @client.search(:query => "type:user created_at>2012-01-01 created_at<2013-01-01 role:end-user -name:Zendesk organization_id:none").page(i).each do |user|
-    @y2012_users << user['id']
-    puts user['id']
+  @client.search(:query => "type:ticket group_id:#{group_id} organization_id:none status:closed updated_at>=2012-01-01 updated_at<2013-01-01").page(i).each do |ticket|
+    @y2012_tickets << ticket['id']
+    puts ticket['id']
   end
 end
 
-File.open("data/y2012_users", "w") { |file| file.write(@y2012_users) }
+File.open("data/#{group_id}/y2012_tickets", "w") { |file| file.write(@y2012_tickets) }
--- lib/zendesk-setup.rb
+++ lib/zendesk-setup.rb
@@ -3,6 +3,8 @@ require 'zendesk_api'
 @client = ZendeskAPI::Client.new do |config|
   config.url = ENV['ZENDESK_URL']
   config.username = ENV['ZENDESK_USER_EMAIL']
-  config.password = ENV['ZENDESK_USER_PASSWORD']
+  # config.password = ENV['ZENDESK_USER_PASSWORD']
+  # config.group_id = ENV['ZENDESK_GROUP']
+  config.token = ENV['ZENDESK_TOKEN']
   config.retry = true
 end
